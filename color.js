150f8c11ec15bc2b5f445fe3615dcd9e3319fefe
hridoye <hridoysagor171@gmail.comm>
2022-09-12 15:14:00 -0600

    unique number

--- color.js
+++ color.js
@@ -1,15 +1,20 @@
 const color_change_btn = document.getElementById("color_change_btn");
 const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple','rgba(0,0,0,80%)'];
+const colors = ['rgba(255, 99, 71, 0.8)', 'green', 'blue', 'rgb(106, 90, 205)', 'pink', 'purple', 'rgba(0,0,0,80%)', 'red', 'grey', 'rgb(139,0,139)'];
 
 body.style.backgroundColor = "violet";
 
 function backgroundChange() {
     color_change_btn.addEventListener("click", function () {
         const randomColor = parseInt(Math.random() * colors.length);
-        console.log("randomColor", randomColor);
         body.style.backgroundColor = colors[randomColor];
     });
 };
 backgroundChange();
+
+const uniqueNumber = [10, 12, 45, 85, 63, 55, 60];
+color_change_btn.addEventListener("click", function () {
+    const randomNumber = parseInt(Math.random() * uniqueNumber.length);
+    console.log("randomNumber", randomNumber);
+});
 
